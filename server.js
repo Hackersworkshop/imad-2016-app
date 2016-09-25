0f848ee529c0e85281138c266a758a9675a65413
nafih <nafihahmd@yahoo.com>
2016-09-25 10:13:08 +0530

    Comm SERVER

--- server.js
+++ server.js
@@ -1,16 +1,55 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
-var app = express();
 app.use(morgan('combined'));
 
+var app = express();
+var cont1={
+   title: 'Article One|HackersWorkhop';
+   heading: 'Article One';
+   content:`
+	 <p>		This is the content of article one.This is the content of article one.This is the content of article one.This is the content of article one.This is the content of article one.This is the content of article one.This is the content of article one.This is the content of article one.This is the content of article one.This is the content of article one.This is the content of article one.This is the content of article one.
+	 </p>
+	 <p>		This is the content of article one.This is the content of article one.This is the content of article one.This is the content of article one.
+	 </p>
+	 <p>		This is the content of article one.This is the content of article one.This is the content of article one.This is the content of article one.
+	 </p>`
+ };
+function crtTemp(data){
+  var title= data.title;
+  var heading= data.heading;
+  var content= var.content;
+  var htmltemp=`
+  <html>
+    <head>
+      <title>
+  	  ${title}
+      </title>
+  	<link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+  	 <div class="bold">
+  		 <a href="/">Home</a>
+  	 </div>
+  	 <hr/>
+  	 <div class=c1>
+    	 <div class="text-big">
+  	    <h1>${heading}
+        </h1>
+  	   </div>
+  	   ${content}
+  	 </div>
+    </body>
+  </html>
+  `;
+  return htmltemp;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article1.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
+  res.sendFile(crtTemp(article1));
 });
 
 app.get('/article2.html', function (req, res) {
