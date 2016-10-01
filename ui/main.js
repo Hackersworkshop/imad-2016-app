c5fd966d2dd8b310b1bf0a9fcab0ad5a61378de4
Nafih <Nafihahmd@yahoo.com>
2016-10-01 08:13:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 var img=document.getElementById('logo');
 function movRght(){
     img.style.marginLeft= '100px';
-};
+}
 img.onclick=function(){
- var intrvl = setintrvl(movRght,100)
+ var intrvl = setintrvl(movRght,100);
   };
 var button=document.getElementById('counter');
