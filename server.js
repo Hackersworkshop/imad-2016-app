6dea82b1d646e669868a69be9dd3d6db7e3befc3
Nafih <Nafihahmd@yahoo.com>
2016-10-01 07:07:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,8 +81,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+app.get('/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js '));
 });
 
 app.get('/:artclnm', function (req, res) {
