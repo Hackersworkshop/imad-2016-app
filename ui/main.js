2d673433ce24b276261925254ad440e710da3c35
Nafih <Nafihahmd@yahoo.com>
2016-10-01 10:53:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,10 +11,10 @@ img.onclick=function(){
   
 var counter=0;
 var button=document.getElementById('counter');
+
 button.onclick=function(){
     var request=new XMLHttpRequest();
-    request.onreadystatechange= function()
-    {
+    httpRequest.onreadystatechange = function(){
        request.readystate == XMLHttpRequest.Done;
     }
     counter=counter+1;
