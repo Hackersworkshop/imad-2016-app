181266ec43ae13ac7f8cc597e9444b58cee9bb79
Nafih <Nafihahmd@yahoo.com>
2016-10-01 08:58:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,8 +7,12 @@ function movRght(){
 img.onclick=function(){
  var interval = setInterval(movRght,10);
   };
+  
+  
 var button=document.getElementById('counter');
 button.onclick = function(){
+    var request=new XMLHttoRequest();
+    count=count+1;
     var span=document.getElementbyId('counter')
     span.InnerHTMl=count.toString();
 }
