62133e53084c8b6bb9ad4f92cb242b0534823e49
Nafih <Nafihahmd@yahoo.com>
2016-10-01 12:51:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,11 +5,11 @@ var button= document.getElementById('counter');
 button.onclick = function() {
               var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
-        if(request.readystate === XMLHttpRequest.DONE){
-         if(request.status === 200){
-             var c = request.responseText;
+        if(request.readystate == XMLHttpRequest.DONE){
+         if(request.status == 200){
+             var counter = request.responseText;
              var span = document.getElementById('count');
-             span.innerHTML = c.toString();
+             span.innerHTML = counter.toString();
          }
         }
     };
