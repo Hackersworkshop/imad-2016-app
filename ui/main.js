826afb2b2fc48df8feed946627127f793dcb71c7
Nafih <Nafihahmd@yahoo.com>
2016-10-04 23:19:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,5 +16,28 @@ button.onclick = function () {
     
     request.open('GET', 'http://hackersworkshop.imad.hasura-app.io/counter', true);
     request.send(null);
+    
+    
+var nameIP=document.GetElementById("name");
+var name=nameIP.value;
+var submit=document.getElementById("submit");
+submit.Onclick function(){
+
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          if (request.status === 200) {
+              var names= request.responseText;
+               names=JSON.parse('names');
+              var list='';      
+              for(var i="0";i=names.length; i++)    {
+             list+='<li>' + names[i]+'</li>'};
+          }
+      }  
+    };
+    
+    request.open('GET', 'http://hackersworkshop.imad.hasura-app.io/submit-n?', true);
+    request.send(null);
 };
 };
