c52d0def7da2343abe65b767089e02415978cf35
Nafih <Nafihahmd@yahoo.com>
2016-10-05 17:46:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.get('/counter', function (req, res) {
 
 var names=[];
 app.get('/submit-n', function(req, res){
-var names=req.query.name;
+var name=req.query.name;
  names.push(name);
 res.send(JSON.stringify(names));
 });
