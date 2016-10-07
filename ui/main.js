ed31f2c4a6b6f0f4d4a3b65c7e08c54cb0be58a9
Nafih <Nafihahmd@yahoo.com>
2016-10-07 22:22:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,10 +9,10 @@ submit.onclick= function(){
       if (request.readyState === XMLHttpRequest.DONE) {
           if (request.status === 200) {
               var names= request.responseText;
-               names=JSON.parse('names');
+               names=JSON.parse('name');
               var list='';      
-              for(var i="0";i < names.length; i++)    {
-             list += '<li>' + names[i]+' </li>' }
+              for(var i="0";i < names.length; i++){
+             list += '<li>' + names[i] + '</li>' }
             var ul=document.getElementById('namelst');
             ul.innerHTML=list;
            }
