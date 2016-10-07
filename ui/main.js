8e1287fb22d9d41121f891c06e28af6d74ad8cd8
Nafih <Nafihahmd@yahoo.com>
2016-10-07 22:03:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ submit.onclick= function(){
       if (request.readyState === XMLHttpRequest.DONE) {
           if (request.status === 200) {
               var names= request.responseText;
-               names=JSON.parse('names');
+               names=JSON.parse('name');
               var list='';      
               for(var i="0";i < names.length; i++)    {
              list=='<li>' + names[i]+'</li>'; }
